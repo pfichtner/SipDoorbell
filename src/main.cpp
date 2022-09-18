1b21f12a91da6801bd18c124255ef71abf19e5a2
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-09-18 19:42:57 +0200

    house keeping

--- src/main.cpp
+++ src/main.cpp
@@ -26,8 +26,6 @@ Sip aSip(acSipOut, sizeof(acSipOut));
 // RCSWITCH
 RCSwitch mySwitch = RCSwitch();
 
-bool saveConfig = false;
-
 // Tasks
 struct task
 {
@@ -39,15 +37,9 @@ task taskA = {.rate = 1000, .previous = 0};
 
 void publishMQTT(void)
 {
-  // MQTTclient.publish
-  /*     MQTTclient.publish(topic.c_str(), char_Leistung_Zaehler4);
-            topic = "Ferraris/";
-            topic = topic + configManager.data.messure_place;
-            topic = topic +"/Zähler4/UKWh";
-            char char_meter_loop_counts4[5];
-            dtostrf(configManager.data.meter_loops_count_4,4,0, char_meter_loop_counts4);
-      MQTTclient.publish(topic.c_str(), char_meter_loop_counts4);
-   */
+  String topic = String(configManager.data.wifi_hostname) + "/sensor/state";
+  MQTTclient.publish(topic.c_str(), "ON");
+  MQTTclient.publish(topic.c_str(), "OFF");
 }
 
 void reconnect(void)
@@ -86,7 +78,49 @@ void setup()
   auto ipAddr = WiFi.localIP().toString().c_str();
   // TODO use random port?
   aSip.Init(configManager.data.sip_server, configManager.data.sip_port, ipAddr, 5060, configManager.data.sip_user, configManager.data.sip_password, configManager.data.sip_ringsecs);
+}
 
+void dial(void)
+{
+  Serial.print("dialing ");
+  Serial.print(configManager.data.sip_numbertodial);
+  Serial.print(" as ");
+  Serial.println(configManager.data.sip_callername);
+  dialingStartedAt = millis();
+  aSip.Dial(configManager.data.sip_numbertodial, configManager.data.sip_callername);
+  Serial.println("dialing done");
+}
+
+void SIPloop(void)
+{
+  aSip.Processing(acSipIn, sizeof(acSipIn));
+}
+
+void RCSwitchLoop(void)
+{
+  if (mySwitch.available())
+  {
+    output(mySwitch.getReceivedValue(), mySwitch.getReceivedBitlength(), mySwitch.getReceivedDelay(), mySwitch.getReceivedRawdata(), mySwitch.getReceivedProtocol());
+    if (mySwitch.getReceivedValue() == configManager.data.rcswitch_value && (configManager.data.rcswitch_protocol < 0 || mySwitch.getReceivedProtocol() == configManager.data.rcswitch_protocol))
+    {
+      Serial.println("rccode matching");
+
+      if (millis() < dialingStartedAt + DIAL_DEBOUNCE_MILLIS)
+      {
+        Serial.println("Dialing already in progress");
+      }
+      else
+      {
+        publishMQTT();
+        dial();
+      }
+    }
+    else
+    {
+      Serial.println("rccode does not match");
+    }
+    mySwitch.resetAvailable();
+  }
 }
 
 void loop()
@@ -97,44 +131,13 @@ void loop()
   configManager.loop();
   MQTTclient.loop();
 
-  aSip.Processing(acSipIn, sizeof(acSipIn));
+  SIPloop();
+  RCSwitchLoop();
 
   // tasks
   if (taskA.previous == 0 || (millis() - taskA.previous > taskA.rate))
   {
     taskA.previous = millis();
     reconnect();
-  }
-
-  if (mySwitch.available())
-  {
-    output(mySwitch.getReceivedValue(), mySwitch.getReceivedBitlength(), mySwitch.getReceivedDelay(), mySwitch.getReceivedRawdata(), mySwitch.getReceivedProtocol());
-    if (mySwitch.getReceivedValue() == configManager.data.rcswitch_value && (configManager.data.rcswitch_protocol < 0 || mySwitch.getReceivedProtocol() == configManager.data.rcswitch_protocol))
-    {
-      Serial.println("rccode matching");
-
-      if (millis() >= dialingStartedAt + DIAL_DEBOUNCE_MILLIS)
-      {
-        Serial.print("dialing ");
-        Serial.print(configManager.data.sip_numbertodial);
-        Serial.print(" as ");
-        Serial.println(configManager.data.sip_callername);
-        aSip.Dial(configManager.data.sip_numbertodial, configManager.data.sip_callername);
-        dialingStartedAt = millis();
-      } else {
-        Serial.println("Dialing already in progress");
-      }
-    }
-    else
-    {
-      Serial.println("rccode does not match");
-    }
-    mySwitch.resetAvailable();
-  }
-
-  if (saveConfig)
-  {
-    saveConfig = false;
-    configManager.save();
   }
 }
