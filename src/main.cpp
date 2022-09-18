e6c62f63f8d67c82a664da9ec746ed87ccf2b2e0
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-09-18 23:28:13 +0200

    debug outs

--- src/main.cpp
+++ src/main.cpp
@@ -70,14 +70,38 @@ void setup()
   WiFi.hostname(configManager.data.wifi_hostname);
   WiFi.begin();
 
-  if (configManager.data.mqtt_server && configManager.data.mqtt_port)
+  if (configManager.data.mqtt_server[0] == '\0' || configManager.data.mqtt_port <= 0)
+  {
+    Serial.println("MQTT not configured");
+  }
+  else
   {
     MQTTclient.setServer(configManager.data.mqtt_server, configManager.data.mqtt_port);
+    Serial.print("Configuring MQTT Broker ");
+    Serial.print(configManager.data.mqtt_server);
+    Serial.print(":");
+    Serial.print(configManager.data.mqtt_port);
   }
 
   auto ipAddr = WiFi.localIP().toString().c_str();
   // TODO use random port?
-  aSip.Init(configManager.data.sip_server, configManager.data.sip_port, ipAddr, 5060, configManager.data.sip_user, configManager.data.sip_password, configManager.data.sip_ringsecs);
+  auto port = 5060;
+
+  Serial.print("Configuring SIP server ");
+  Serial.print(configManager.data.sip_server);
+  Serial.print(":");
+  Serial.print(configManager.data.sip_port);
+  Serial.print(" using ");
+  Serial.print(configManager.data.sip_user);
+  Serial.print("@");
+  Serial.print(configManager.data.sip_password);
+  Serial.print(" (local ");
+  Serial.print(ipAddr);
+  Serial.print(":");
+  Serial.print(port);
+  Serial.println(")");
+
+  aSip.Init(configManager.data.sip_server, configManager.data.sip_port, ipAddr, port, configManager.data.sip_user, configManager.data.sip_password, configManager.data.sip_ringsecs);
 }
 
 void switchPin(boolean state)
